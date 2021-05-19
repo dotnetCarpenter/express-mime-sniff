e3cec9bf172de6d684b8725d95cac3de3648db4b
dotnetCarpenter <jon.ronnenberg@gmail.com>
2021-05-19 19:25:53 +0200

    print only the mime-type data

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import { spawn }    from 'child_process';
+import S            from 'sanctuary';
 import eventEmitter from './eventEmitter.js';
 
-// const sniffer = file => 
+// const trace = tag => x => (console.log (tag, x), x)
+const sliceAfterSpace = S.ap (s => n => s.slice (n + 1)) (s => s.indexOf (' '));
 
-const observer = eventEmitter (new Map)
-const file = spawn('file', ['-i', './tests/fixtures/t-ssm.jpg']);
+const sniffer = path => ({
+  ...eventEmitter (new Map),
 
+  mimeType () {
+    const file = spawn('file', ['--mime', path]);
+    const trigger = name => args => this.trigger (name, args);
+
+    file.stdout.on ('data', S.pipe ([
+      sliceAfterSpace,
+      trigger ('mime'),
+    ]));
+  },
+})
+
+
+// test
+// FIXME: this API sucks! We do not want to set `on` manually but rather as a second argument
+const getMimeType = sniffer ('./tests/fixtures/t-ssm.jpg');
+getMimeType.on ('mime', S.compose (console.log) (String));
+getMimeType.mimeType ();
+
+/*
 file.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
@@ -18,4 +39,4 @@ file.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
 });
 
-
+*/
